b02866bb35ccd0499c6a62b7ba91feb84aeb358f
WellingtonAds <ads231n2@gmail.com>
2024-02-01 16:02:24 +0000

    Changes to be committed: modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Wellington Uchôa PInheiro",
+  title: "Analista de Sistemas",
+  email: "ideiacode@ideiacode.com",
+  gitHub: "WellingtonADS",
+  instagram: "wellington.uchoa.pinheiro",
+  linkedIn: "wpinheiro-ads",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
